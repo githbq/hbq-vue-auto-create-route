1c3cf862f34af28778988401caac8c6e69ccf930
hubiqing <hbqhbq123@qq.com>
2020-09-17 15:47:18 +0800

    pluginify

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ const core = require('./core')
 // 生产模式不监听
 if (process.env.NODE_ENV !== 'production') {
   // usePolling 开启路径轮询扫描,对文件夹名称修改仍能触发监听 ; interval 轮询间隔毫秒时长
-  chokidar.watch('src/**/meta.json', { usePolling: true, interval: 500 }).on('all', () => {
-    core.run()
-  })
+  chokidar.watch('src/**/meta.json', { usePolling: true, interval: 500 }).on('all', core.run)
 } else {
   core.run()
 }
